4715091b5145133f720a843b438b546bdee1e1d3
tomitawataru <tomitawataru@tomitawatarunoMacBook-Pro.local>
2017-07-10 15:31:10 +0900

    点滅

---
+++ Etest.java
@@ -0,0 +1,6 @@
+public class Etest{
+	public static void main(String[] args) {
+		LightClass LightC = new LightClass();
+		LightC.OnLight();
+	}
+}
---
+++ LightClass.java
@@ -0,0 +1,8 @@
+class LightClass{
+	public void OnLight(){
+		System.out.println("点灯");
+	}
+	public void OffLight(){
+		System.out.println("消灯");
+	}
+}
